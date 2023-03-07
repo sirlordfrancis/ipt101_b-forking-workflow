671a21da4d30331a59dd3f7cb34d36c22f7ef17a
jhun-BSIT-2 <coritico.jhunziel@hnu.edu.ph>
2023-03-07 16:09:53 +0800

    Added new Function

--- forking-workflow.c
+++ forking-workflow.c
@@ -9,11 +9,16 @@ int main(){
 	printf("Your name must be printed if this program is run. \n\n");
 	
 	// Call your function below this line.
-	
+	cesar_printName();
 	return 0;
 }
 
 
 // Refer to the function name for the naming of your funcion.
 void navarro_printName() {
+}
+
+void cesar_printName(){
+	
+	printf("jeffrey Cesar");
 }
