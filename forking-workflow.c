05eb98f5db3fad6398cf56bc7e3bbfde8882c3ed
Zachary Legaria <gmzach024@gmail.com>
2023-03-07 16:54:39 +0800

    added new function

--- forking-workflow.c
+++ forking-workflow.c
@@ -9,11 +9,15 @@ int main(){
 	printf("Your name must be printed if this program is run. \n\n");
 	
 	// Call your function below this line.
-	
+	saycon_printName();
 	return 0;
 }
 
 
 // Refer to the function name for the naming of your funcion.
 void navarro_printName() {
+}
+
+	void saycon_printName(){
+	printf("Karl Christian Saycon");
 }
