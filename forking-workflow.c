c97ad628fe57f42b1763ac7e206710c86d078cba
ace-kckc <acedo.kaedei@hnu.edu.ph>
2023-03-07 16:17:02 +0800

    Ferlyz Llander

--- forking-workflow.c
+++ forking-workflow.c
@@ -9,7 +9,7 @@ int main(){
 	printf("Your name must be printed if this program is run. \n\n");
 	
 	// Call your function below this line.
-	
+	llander_printName();
 	return 0;
 }
 
@@ -17,3 +17,7 @@ int main(){
 // Refer to the function name for the naming of your funcion.
 void navarro_printName() {
 }
+void llander_printName() {
+	printf("Ferlyz Jan B. LLander")
+}
+
