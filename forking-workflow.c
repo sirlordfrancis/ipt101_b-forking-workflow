793d78f16eb93e6b08e2cc813e37eeeb537d3158
hannahleiamba <amba.hannah_lei@hnu.edu.ph>
2023-03-07 16:18:33 +0800

    binangbang-fork

--- forking-workflow.c
+++ forking-workflow.c
@@ -9,6 +9,7 @@ int main(){
 	printf("Your name must be printed if this program is run. \n\n");
 	
 	// Call your function below this line.
+	binangbang_printName();
 	
 	return 0;
 }
@@ -16,4 +17,8 @@ int main(){
 
 // Refer to the function name for the naming of your funcion.
 void navarro_printName() {
+}
+
+void binangbang_printName() {
+	printf("Kelvin Keith L. Binangbang");
 }
