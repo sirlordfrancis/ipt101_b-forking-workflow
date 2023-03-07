5383678ec59431d69c75d152dbc4be12379d1d79
stephaniesamuya <haniesamuya03@gmail.com>
2023-03-07 16:26:32 +0800

    Added a new function - Samuya

--- forking-workflow.c
+++ forking-workflow.c
@@ -9,11 +9,13 @@ int main(){
 	printf("Your name must be printed if this program is run. \n\n");
 	
 	// Call your function below this line.
+	samuya_printName();
 	
 	return 0;
 }
 
 
 // Refer to the function name for the naming of your funcion.
-void navarro_printName() {
+void samuya_printName() {
+	printf("Stephanie Lou B. Samuya");
 }
