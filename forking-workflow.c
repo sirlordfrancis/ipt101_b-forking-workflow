9d16f6cfed719e3dbc7b2760d82bc863a8fadb85
peelieree <pilayre.marvin_ray@hnu.edu.ph>
2023-03-07 16:05:13 +0800

    Marvin

--- forking-workflow.c
+++ forking-workflow.c
@@ -9,11 +9,14 @@ int main(){
 	printf("Your name must be printed if this program is run. \n\n");
 	
 	// Call your function below this line.
-	
+	name();
 	return 0;
 }
 
 
 // Refer to the function name for the naming of your funcion.
 void navarro_printName() {
+}
+void name(){
+	printf("Marvin Ray Pilayre");
 }
