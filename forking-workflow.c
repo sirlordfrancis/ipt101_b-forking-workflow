a8103cada796875402ef10053682633781be896d
Ronald <amba.hannah_lei@hnu.edu.ph>
2023-03-07 16:15:38 +0800

    added new function

--- forking-workflow.c
+++ forking-workflow.c
@@ -9,11 +9,15 @@ int main(){
 	printf("Your name must be printed if this program is run. \n\n");
 	
 	// Call your function below this line.
-	
+	pinos_printName();	
 	return 0;
 }
 
 
 // Refer to the function name for the naming of your funcion.
 void navarro_printName() {
+}
+
+void pinos_printName() {
+	printf("Arjay Pinos");
 }
