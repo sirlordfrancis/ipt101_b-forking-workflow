b2094946d165f5ab5fd1693277ea06e33bffec01
jundexy111 <jundexy111@gmail.com>
2023-03-07 16:04:53 +0800

    added name function

--- forking-workflow.c
+++ forking-workflow.c
@@ -9,6 +9,7 @@ int main(){
 	printf("Your name must be printed if this program is run. \n\n");
 	
 	// Call your function below this line.
+	pogio_printName();
 	
 	return 0;
 }
@@ -17,3 +18,8 @@ int main(){
 // Refer to the function name for the naming of your funcion.
 void navarro_printName() {
 }
+
+void pogio_printName() {
+	printf("Nouie Kenneth Pogio");
+}
+
